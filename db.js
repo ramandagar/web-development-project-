0fbb4895f889ef39fad0bb141c048bc3fbd48ce4
ramandagar <89700171+ramandagar@users.noreply.github.com>
2022-06-02 23:07:43 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,20 @@
+//open database
+//create objectstore
+//Make 
+let db;
+let openRequest = indexedDB.open("myDatabase");
+openRequest.addEventListener("success",(e)=>{
+    console.log("DB Success");
+    db = openRequest.result;
+})
+openRequest.addEventListener("error",(e)=>{
+    console.log("DB error");
+})
+openRequest.addEventListener("upgradeneeded",(e)=>{
+    console.log("DB upgraded and also for initial DB creation");
+    db = openRequest.result;
+
+    db.createObjectStore("video",{keyPath:"id"});
+    db.createObjectStore("image",{keyPath:"id"});
+
+})
